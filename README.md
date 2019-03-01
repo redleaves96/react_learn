3b7da936c9785f77296f46c33f76d2746f5735b7
finyle <834596036@qq.com>
2019-03-01 23:37:37 +0800

    redux

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-使用Antd实现TodoList页面布局
+1.使用Antd实现TodoList页面布局
+2.使用Redux实现TodoList的添加，删除
+
--- src/TodoList.js
+++ src/TodoList.js
@@ -1,33 +1,69 @@
 import React, { Component } from 'react';
 import 'antd/dist/antd.css';
 import { Input, Button, List } from 'antd';
+import store from './store/index';
 
-const data = [
-    'Racing car sprays burning fuel into crowd.',
-    'Japanese princess to wed commoner.',
-    'Australian walks 100km after outback crash.',
-    'Man charged over missing wedding girl.',
-    'Los Angeles battles huge wildfires.',
-  ];
-  
+
 class TodoList extends Component {
-    
+
+    constructor(props) {
+        super(props);
+        this.state = store.getState();
+        this.handleInputChange = this.handleInputChange.bind(this);
+        this.handleStoreChange = this.handleStoreChange.bind(this);
+        this.handleBtnClick = this.handleBtnClick.bind(this);
+        store.subscribe(this.handleStoreChange);
+    }
+
     render() {
         return (
             <div style={{marginTop: '10px', marginLeft: '10px'}}>
                 <div>
-                    <Input placeholder='todo info' style={{width:'300px' ,marginRight: '10px'}}/>
-                    <Button type="primary">提交</Button>
+                    <Input 
+                    value={this.state.inputValue} 
+                    placeholder='todo info' 
+                    style={{width:'300px' ,marginRight: '10px'}}
+                    onChange={this.handleInputChange}
+                    />
+                    <Button type="primary" onClick={this.handleBtnClick}>提交</Button>
                 </div>
                 <List
                 style={{marginTop: '10px', width: '300'}}
                 bordered
-                dataSource={data}
-                renderItem={item => (<List.Item>{item}</List.Item>)}
+                dataSource={ this.state.list }
+                renderItem={(item, index)=> (<List.Item onClick={this.handleItemDelete.bind(this, index)}>{item}</List.Item>)}               
                 />  
             </div>
         );
         
+    }
+
+    handleInputChange(e) {
+        const action = {
+            type: 'change_input_value',
+            value: e.target.value
+        }
+        store.dispatch(action);
+    }
+
+    handleStoreChange() {
+        this.setState(store.getState());
+
+    }
+
+    handleBtnClick() {
+        const action = {
+            type: 'add_todo_item'
+        };
+        store.dispatch(action);
+    }
+    
+    handleItemDelete(index) {
+        const action = {
+            type: 'delete_todo_item',
+            index
+        }
+        store.dispatch(action);
     }
 }
 
